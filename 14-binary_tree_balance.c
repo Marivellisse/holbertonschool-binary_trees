2a650eba1d497b4bbec0c234a1e4540c89009b4a
Marivellisse <gmarivellisse@gmail.com>
2025-04-12 22:03:30 -0400

    14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -4,7 +4,7 @@
  * binary_tree_height - Measures the height of a binary tree
  * @tree: Pointer to the root node
  *
- * Return: Height or 0 if tree is NULL
+ * Return: Height of the tree or 0 if tree is NULL
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
@@ -18,23 +18,25 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree->right)
 		right_h = 1 + binary_tree_height(tree->right);
 
-	if (left_h > right_h)
-		return (left_h);
-	else
-		return (right_h);
+	return (left_h > right_h ? left_h : right_h);
 }
 
 /**
  * binary_tree_balance - Measures the balance factor of a binary tree
  * @tree: Pointer to the root node
  *
- * Return: Balance factor, or 0 if tree is NULL
+ * Return: Balance factor or 0 if tree is NULL
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	int left_h = 0, right_h = 0;
+
 	if (tree == NULL)
 		return (0);
 
-	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+	left_h = binary_tree_height(tree->left);
+	right_h = binary_tree_height(tree->right);
+
+	return (left_h - right_h);
 }
 
